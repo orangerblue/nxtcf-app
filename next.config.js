bc31eeccb55c4b7d733a86349c8e2447c0d0e1bd
danhenly <danhenlysales@gmail.com>
2023-02-11 15:20:23 +0800

    remove sitewide experimental edge

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+
 const nextConfig = {
-  experimental: {
-    runtime: 'experimental-edge',
-  },
   reactStrictMode: true,
   swcMinify: true,
 }
 
+
+/*  experimental: {
+    runtime: 'edge',
+  }*/
 module.exports = nextConfig
